a77e6ea03aa0056e7c4d323f15c26b61ae721ad6
M.Yüksel Koyunoglu <nykoyunoglu@gmail.com>
2024-05-05 17:27:57 +0200

    added props with

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script>
+  export default {
+    name: "Foods",
+    data() {
+      return {
+        foods: [
+          { name: 'Apples', desc: 'Apples are a type of fruit that grow on trees.', favorite: true},
+          { name: 'Pizza', desc: 'Pizza has a bread base with tomato sauce, cheese, and toppings on top.', favorite: false},
+          { name: 'Rice', desc: 'Rice is a type of grain that people like to eat.', favorite: false},
+          { name: 'Fish', desc: 'Fish is an animal that lives in water.', favorite: true},
+          { name: 'Cake', desc: 'Cake is something sweet that tates good.', favorite: false}
+        ]
+      }
+    },
+    methods: {
+      recieveEmit(foodId) {
+        let foundFood = this.foods.find( x => x.name === foodId)
+        foundFood.favorite = !foundFood.favorite
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="w3-container">
+    <h1>Foods</h1>
+    <div class="w3-container wrapper">
+      <food-item
+        v-for="x in foods"
+        :key="x.name"
+        :food-name="x.name"
+        :food-desc="x.desc"
+        :is-favorite="x.favorite"
+        @toggle-favorite="recieveEmit"
+      ></food-item>
+    </div>
+  </div>
+</template>
+
+<style>
+  .wrapper {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .wrapper > div {
+    flex-basis: 200px;
+    margin: 10px;
+    padding: 10px;
+    border: dashed black 1px;
+    background-color: lightgreen;
+  }
+</style>
---
+++ src/components/FoodItem.vue
@@ -0,0 +1,39 @@
+<script>
+    export default {
+        props: {
+            foodName: {
+                type: String,
+                required: true,
+                default: "Food Name"
+            },
+            foodDesc: {
+                type: String,
+                required: true,
+                default: "Food Desc"
+            },
+            isFavorite: {
+                type: Boolean,
+                required: false
+            }
+        },
+        methods: {
+            toggleFavorite() {
+                this.$emit("toggle-favorite", this.foodName)
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="w3-container">
+        <h2>
+            {{ foodName }}
+            <img src="../components/icons/img_quality.svg" alt="" v-show="isFavorite" style="height: 1.5em; float: right;">
+        </h2>
+        <p>{{ foodDesc }}</p>
+
+        <button @click="toggleFavorite" class="w3-button w3-black w3-hover-red w3-round-large">Favorite</button>
+    </div>
+</template>
+
+<style></style>
